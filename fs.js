d635912c0b54e35abbf1c9415f4b4bc92dd1cf14
Celinafernandes9 <celina.922.fernz@gmail.com>
2023-10-16 19:34:49 +0530

    upadted/append file

--- fs.js
+++ fs.js
@@ -3,9 +3,23 @@ const http = require('http');
 const fs = require('fs');
 
 http.createServer((req, res)=>{
-    fs.readFile("server.js" , (err, data)=>{
-    res.writeHead(200, {'content-type':'text/plain'});
-     res.write(data)   
-    res.end()
-    })
-}).listen(4000);
+
+// file reading operation
+
+    // fs.readFile("server.js" , (err, data)=>{
+    // res.writeHead(200, {'content-type':'text/plain'});
+    //  res.write(data)   
+    // res.end()
+    // })
+
+// appending file
+// In append file the data is added at the last line of the code
+
+    // fs.appendFile('index.html','<h2>This is append file</h2>',(err, data)=>{
+    //     if(err) throw err
+    //     console.log("File updated");
+    //     return res.end()
+    // })
+
+
+}).listen(4000); 
