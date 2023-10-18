8b0c772e0e4d825635ef0f46e484b27e91ea62d8
Celinafernandes9 <celina.922.fernz@gmail.com>
2023-10-18 12:10:19 +0530

    read,write, append and close mode

---
+++ file_mode.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+
+// read and write mode
+
+// fs.open('example.txt', 'r+', (err , data)=>{
+//     if(err){
+//         console.error("Error:" , err);
+//         return;
+//     }
+//     console.log("File open for reading and writing mode");
+// });
+
+// append file
+
+fs.open("example.txt", "a+", (err, data) => {
+  if (err) {
+    console.log("Error while appending file: ", err);
+    return;
+  }
+  console.log("File append successfully");
+
+// closing file 
+ 
+  fs.close(data, (err)=>{
+    if(err){
+        console.error("Error closing the file :" + err)
+        return;
+    }
+    console.log("file closed");
+  })
+});
