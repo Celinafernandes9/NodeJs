c4062e99c87a9d64cf67a8c022f841a6f362a436
Celinafernandes9 <celina.922.fernz@gmail.com>
2023-10-20 00:40:32 +0530

    using  middleware and importing router/updated

--- express.js
+++ express.js
@@ -4,6 +4,13 @@ const app = express();
 // for connecting db
 const mongoose = require('mongoose');
 
+app.use(express.json());
+const router = require('./route');
+app.use("/user", router);
+
+// app.use(express.static('public'))
+
+// connect to MongoDB database using Mongoose
 mongoose.set("strictQuery", true)
 mongoose.connect("mongodb+srv://Celina:qazplm@cluster0.mvphbd4.mongodb.net/Nodejs?retryWrites=true&w=majority")
 
@@ -21,12 +28,12 @@ mongoose.connect("mongodb+srv://Celina:qazplm@cluster0.mvphbd4.mongodb.net/Nodej
 
 // creating simple api
 // to access data
-app.get('/', (req, res)=>{
-    res.send("Hello World!");
-})
+// app.get('/', (req, res)=>{
+//     res.send("Hello World!");
+// })
 
 // to see the output we use postman
-app.post('/post',(req, res)=>{
-    res.send("This is post method")
-})
+// app.post('/post',(req, res)=>{
+//     res.send("This is post method")
+// })
 app.listen(4000);
