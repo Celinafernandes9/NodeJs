ed278b57cadf734a858b90bdd14b1281e0b39971
Celinafernandes9 <celina.922.fernz@gmail.com>
2023-10-19 18:46:20 +0530

    connecting to db

--- express.js
+++ express.js
@@ -1,6 +1,23 @@
 // for creating api we use express Third Party Package
 const express = require('express');
 const app = express();
+// for connecting db
+const mongoose = require('mongoose');
+
+mongoose.set("strictQuery", true)
+mongoose.connect("mongodb+srv://Celina:qazplm@cluster0.mvphbd4.mongodb.net/Nodejs?retryWrites=true&w=majority")
+
+.then(()=>{
+    console.log("connected to MongoDB");
+})
+.catch((err)=>{
+    console.log(`Error : ${err}`);
+})
+
+// (err, data)=>{
+//     if(err) throw err
+//     console.log("Connection created");
+// }
 
 // creating simple api
 // to access data
