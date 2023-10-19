3e0cb0148e238ec91f0b4db0fd26a56b5abbfe4c
Celinafernandes9 <celina.922.fernz@gmail.com>
2023-10-19 14:31:13 +0530

    file existing

--- file_mode.js
+++ file_mode.js
@@ -1,50 +1,5 @@
 const fs = require("fs");
 
-// read and write mode
-
-// fs.open('example.txt', 'r+', (err , data)=>{
-//     if(err){
-//         console.error("Error:" , err);
-//         return;
-//     }
-//     console.log("File open for reading and writing mode");
-// });
-
-// append file
-
-// fs.open("example.txt", "a+", (err, data) => {
-//   if (err) {
-//     console.log("Error while appending file: ", err);
-//     return;
-//   }
-//   console.log("File append successfully");
-
-// closing file 
- 
-//   fs.close(data, (err)=>{
-//     if(err){
-//         console.error("Error closing the file :" + err)
-//         return;
-//     }
-//     console.log("file closed");
-//   })
-// });
-
-// write file
-// in writefile  if the dile is not craeted it will create and give file automatically
-
-// fs.writeFile("new.txt", "helloo world..", (err)=>{
-//   if(err) throw err
-//   console.log("saved");
-// })
-
-// append file
-
-// fs.appendFile("new.txt", "helloo world.. this is append file", (err)=>{
-//   if(err) throw err
-//   console.log("updated");
-// })
-
 // use to check existence file/folder
 getCurrentFile();
 
